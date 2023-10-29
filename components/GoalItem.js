0593ad9bff736f6c9059393c43954dad16e2baf0
nalymoslih <naly.moslih48@gmail.com>
2023-10-30 01:05:05 +0300

    push

---
+++ components/GoalItem.js
@@ -0,0 +1,21 @@
+import {View, Text, FlatList} from 'react-native';
+import React from 'react';
+
+const GoalItem = props => {
+  <View
+    style={{
+      margin: 8,
+      padding: 8,
+      borderRadius: 8,
+      backgroundColor: '#5E0acc',
+    }}>
+    <Text
+      style={{
+        color: 'white',
+      }}>
+      {props.text}
+    </Text>
+  </View>;
+};
+
+export default GoalItem;
