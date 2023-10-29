0914a302e56241e0babcb53e3922c04359f5b921
nalymoslih <naly.moslih48@gmail.com>
2023-10-30 00:30:54 +0300

    push

--- App.js
+++ App.js
@@ -18,52 +18,70 @@ const App = () => {
   };
 
   const addGoalHandler = () => {
-    setCourseGoals(currentCourseGoals => [...courseGoals, enterTheGoalText]);
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals,
+      enterTheGoalText,
+    ]);
   };
 
   return (
-    <ScrollView>
+    <View
+      style={{
+        flex: 1,
+        marginVertical: 60,
+        marginHorizontal: 20,
+      }}>
       <View
         style={{
-          padding: 70,
-          justifyContent: 'center',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
         }}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
-          <TextInput
-            placeholder="Enter the goal"
-            style={styles.textInput}
-            onChangeText={goalInputHandler}
-            value={enterTheGoalText}
-          />
-          <Button title="Add" onPress={addGoalHandler} />
-        </View>
-        <View
-          style={{
-            marginTop: 20,
-            // backgroundColor: '#ccc',
-          }}>
-          {courseGoals.map(goal => (
-            <Text
-              style={{
-                padding: 10,
-                marginVertical: 10,
-                backgroundColor: 'white',
-                borderColor: 'black',
-                borderWidth: 1,
-                backgroundColor: '#ccc',
-              }}
-              key={goal}>
-              {goal}
-            </Text>
-          ))}
-        </View>
+        <TextInput
+          placeholder="Enter the goal"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enterTheGoalText}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
-    </ScrollView>
+
+      <View
+        style={{
+          borderBottomWidth: 1,
+          borderBottomColor: '#ccc',
+          marginTop: 20,
+          width: '90%',
+          // backgroundColor: '#ccc',
+        }}
+      />
+
+      <View
+        style={{
+          marginTop: 20,
+          // backgroundColor: '#ccc',
+        }}>
+        <ScrollView alwaysBounceVertical={false}>
+          {courseGoals.map(goal => (
+            <View
+              style={{
+                margin: 8,
+                padding: 8,
+                borderRadius: 8,
+                backgroundColor: '#5E0acc',
+              }}>
+              <Text
+                style={{
+                  color: 'white',
+                }}
+                key={goal}>
+                {goal}
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
   );
 };
 
